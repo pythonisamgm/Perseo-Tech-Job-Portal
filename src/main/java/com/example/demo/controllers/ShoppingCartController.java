d510fbecd70eadb2b182e703a231bdcc6c35827b
Marta Gonzalez Morales <pythonisamgm@gmail.com>
2024-09-12 09:54:58 +0200

    feat: add methods for adding and removing courses from shopping cart

--- src/main/java/com/example/demo/controllers/ShoppingCartController.java
+++ src/main/java/com/example/demo/controllers/ShoppingCartController.java
@@ -5,6 +5,7 @@ import com.example.demo.dto.shoppingCart.ShoppingCartDTO;
 import com.example.demo.models.ShoppingCart;
 import com.example.demo.services.ShoppingCartServiceImpl;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -81,6 +82,18 @@ public class ShoppingCartController {
     public ResponseEntity<Void> deleteAllShoppingCarts() {
         shoppingCartService.deleteAllShoppingCarts();
         return ResponseEntity.noContent().build();
+    }
+    @PutMapping ("{cartId}/courses/{courseId}")
+    public ResponseEntity<ShoppingCartDTO> addCourseToCart(@PathVariable Long cartId, @PathVariable Long courseId){
+        ShoppingCart updatedCart = shoppingCartService.addCourseToCart(cartId, courseId);
+        ShoppingCartDTO updatedCartDTO = shoppingCartConverter.shoppingCartToDto(updatedCart);
+        return ResponseEntity.ok(updatedCartDTO);
+    }
+    @DeleteMapping("/{cartId}/courses/{courseId}")
+    public ResponseEntity<ShoppingCartDTO> removeCourseFromCart(@PathVariable Long cartId, @PathVariable Long courseId){
+        ShoppingCart updatedCart = shoppingCartService.removeCourseFromCart(cartId, courseId);
+        ShoppingCartDTO updatedCartDTO = shoppingCartConverter.shoppingCartToDto(updatedCart);
+        return ResponseEntity.ok(updatedCartDTO);
     }
 }
 
