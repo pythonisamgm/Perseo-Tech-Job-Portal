16cf8fdf11d37cabb9be5fec04bf5f2bad317a49
Marta Gonzalez Morales <pythonisamgm@gmail.com>
2024-09-19 13:22:12 +0200

    remove custom functions from irepository

--- src/main/java/com/example/demo/repositories/IShoppingCartRepository.java
+++ src/main/java/com/example/demo/repositories/IShoppingCartRepository.java
@@ -1,11 +1,14 @@
 package com.example.demo.repositories;
 
+import com.example.demo.models.Course;
 import com.example.demo.models.ShoppingCart;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface IShoppingCartRepository extends JpaRepository<ShoppingCart, Long> {
-    public ShoppingCart addCourseToCart(Long cartId, Long userId);
-    public double calculateTotalAmount(Long cartId);
 }
